b0c1860fdb82371bae2c5f39ccbc0cb72f96e9ed
ManarSayedEid <manarsayed1994@gmail.com>
2021-12-15 10:08:10 +0200

    add description

---
+++ src/app.js
@@ -0,0 +1,4 @@
+// Write a function that receives month number and year as parameters and returns
+// the number of days for the given month.
+// The function should compute correct days of February.
+
