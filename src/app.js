2adb9891c6f3199e486cf808c6d6382800fdb85d
ManarSayedEid <manarsayed1994@gmail.com>
2021-12-16 03:29:35 +0200

    initiate data-processing challenge

---
+++ src/app.js
@@ -0,0 +1,5 @@
+// Implement the following functions for filtering data from movies.js file:
+// • Print title of all the movies starting in a given year.
+// • Print all the movies for a given director.
+// • Print all movies for a given genre.
+
