26043979778a865d7ee985605cf827b90067cf16
ManarSayedEid <manarsayed1994@gmail.com>
2021-12-23 09:53:00 +0200

    add for of / forEach

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@
 class Todo {
 
     constructor() {
-
         this.taskList = [];
     }
 
@@ -10,47 +9,33 @@ class Todo {
     // If the priority is missing the value 0 will be used instead.
     // Also, the name of the tasks must be unique.
     addTask(name, priority = 0) {
-
-        let taskList = this.taskList;
-        let isExist = false;
-        for (let i = 0; i < taskList.length; i++) {
-            if (name === taskList[i].name) {
-                isExist = true;
+        for (let task of this.taskList) {
+            if (name === task.name) {
                 console.log('This task is alraedy exists')
-                break;
+                return;
             }
         }
-        if (!isExist) {
-            taskList.push({ name, priority });
-        }
+        this.taskList.push({ name, priority });
     }
 
     // • Should remove tasks with the specified name. A status message should be printed in the console.
     removeTaskByName(name) {
-
-        let taskList = this.taskList;
-        let isDeleted = false;
-        for (let i = 0; i < taskList.length; i++) {
-            if (name === taskList[i].name) {
-                taskList.splice(i, 1);
-                isDeleted = true;
+        this.taskList.forEach((task, index) => {
+            if (name === task.name) {
+                this.taskList.splice(index, 1);
                 console.log('Task deleted successfully!');
+                return;
             }
-        }
-        if (!isDeleted) {
-            console.log('This task does not exist!');
-        }
+        })
     }
 
     // • Should print all the tasks.
     printAllTask() {
-
         this.taskList.forEach((task) => console.log('Task name:', task.name, 'Priority:', task.priority));
     }
 
     // • Should print the tasks filtered by a call back function received as parame-ter.
     printTask(value) {
-
         if (typeof value === 'string') {
             this.printfilteredTask(task => task.name === value);
         }
@@ -61,14 +46,12 @@ class Todo {
     }
 
     printfilteredTask(callback) {
-
         console.log(this.taskList.filter(callback));
     }
 
 
     // • Should sort the tasks ascending by name
     sortByName() {
-
         this.taskList.sort((currentTask, nextTask) => {
             if (currentTask.name < nextTask.name) return -1;
             if (currentTask.name > nextTask.name) return 1;
@@ -78,7 +61,6 @@ class Todo {
 
     // • Should sort the tasks ascending by priority
     sortByPriority() {
-
         this.taskList.sort((currentTask, nextTask) => currentTask.priority - nextTask.priority);
     }
 }
